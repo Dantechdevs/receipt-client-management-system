41f8387a0de2ba6246e07cdfb9f149eee6a55591
Daniel Ngwasi <damnngwasi@gmail.com>
2026-06-27 15:00:00 +0300

    Add session-based auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+function requireAuth(req, res, next) {
+  if (!req.session.user) {
+    return res.redirect('/login');
+  }
+  res.locals.currentUser = req.session.user;
+  next();
+}
+
+function requireRole(...roles) {
+  return (req, res, next) => {
+    if (!req.session.user) return res.redirect('/login');
+    if (!roles.includes(req.session.user.role)) {
+      return res.status(403).render('error', { message: 'You do not have permission to access this page.' });
+    }
+    next();
+  };
+}
+
+module.exports = { requireAuth, requireRole };
